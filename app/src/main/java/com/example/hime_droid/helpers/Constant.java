68a4fcd5fb011525e45c5bdd0d027bffba14b619
phnaharris <phnanh.harris@gmail.com>
2023-02-23 14:55:04 +0700

    feat(release): temporary full feature, worst UI i ever make

---
+++ app/src/main/java/com/example/hime_droid/helpers/Constant.java
@@ -0,0 +1,6 @@
+package com.example.hime_droid.helpers;
+
+public class Constant {
+//    public String API_DOMAIN = "https://6820-27-78-200-229.ap.ngrok.io"; // main
+    public String API_DOMAIN = "https://542f-14-161-2-192.ap.ngrok.io"; // personal
+}
